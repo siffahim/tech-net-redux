e7f00c0132825f089a59273e54d7dc9e9e1a1d95
siffahim <siffahim25@gmail.com>
2023-12-15 18:17:37 +0600

    feat: tech net redux

---
+++ src/redux/features/cart/cartSlice.ts
@@ -0,0 +1,48 @@
+import { IProduct } from '@/types/globalTypes';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+interface ICart {
+  products: IProduct[];
+}
+
+const initialState: ICart = {
+  products: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<IProduct>) => {
+      const existing = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+      if (existing) {
+        existing.quantity = existing.quantity! + 1;
+      } else {
+        state.products.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeOne: (state, action: PayloadAction<IProduct>) => {
+      const existing = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+      if (existing && existing.quantity! > 1) {
+        existing.quantity = existing.quantity! - 1;
+      } else {
+        state.products = state.products.filter(
+          (product) => product._id !== action.payload._id
+        );
+      }
+    },
+    removeFromCart: (state, action: PayloadAction<IProduct>) => {
+      state.products = state.products.filter(
+        (product) => product._id !== action.payload._id
+      );
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, removeOne } = cartSlice.actions;
+export default cartSlice.reducer;
